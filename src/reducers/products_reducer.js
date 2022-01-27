adcbf1dd99d2bb9445ac36131e9111ba7afadc25
Thai To <quocthaitl@gmail.com>
2022-01-27 23:29:06 +0700

    Setup fetched products context and reducer

--- src/reducers/products_reducer.js
+++ src/reducers/products_reducer.js
@@ -16,6 +16,27 @@ const products_reducer = (state, action) => {
 	if (action.type === SIDEBAR_CLOSE) {
 		return { ...state, isSidebarOpen: false };
 	}
+
+	if (action.type === GET_PRODUCTS_BEGIN) {
+		return { ...state, productsIsLoading: true };
+	}
+
+	if (action.type === GET_PRODUCTS_SUCCESS) {
+		const productsFeatured = action.payload.filter(
+			(product) => product.featured === true
+		);
+		return {
+			...state,
+			productsIsLoading: false,
+			products: action.payload,
+			productsFeatured,
+		};
+	}
+
+	if (action.type === GET_PRODUCTS_ERROR) {
+		return { ...state, productsIsLoading: false, productsIsError: true };
+	}
+
 	return state;
 	throw new Error(`No Matching "${action.type}" - action type`);
 };
