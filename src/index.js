eea40b2a2e92360f5400071e32520c4109a83684
Thai To <quocthaitl@gmail.com>
2022-02-07 16:55:41 +0700

    Add env file

--- src/index.js
+++ src/index.js
@@ -13,8 +13,8 @@ import { Auth0Provider } from "@auth0/auth0-react";
 
 ReactDOM.render(
 	<Auth0Provider
-		domain="dev-3ajyjnwk.us.auth0.com"
-		clientId="ElIj8BjUG3lTirpMe5x1H7HSh5Gsowjp"
+		domain={process.env.REACT_APP_AUTH_DOMAIN}
+		clientId={process.env.REACT_APP_AUTH_CLIENT_ID}
 		redirectUri={window.location.origin}
 		cacheLocation="localstorage"
 	>
